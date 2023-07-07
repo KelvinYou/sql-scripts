3231dfde30d709e3eff45e190e96ad337c718cff
Kelvin <ykekelvin0220@gmail.com>
2023-07-07 14:22:33 +0800

    update

---
+++ general/general.sql
@@ -0,0 +1,12 @@
+-- oracle sql
+
+-- select all item in table_name
+
+SELECT * FROM table_name
+
+
+-- update the previous info
+
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
